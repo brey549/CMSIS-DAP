25e26f63453650d865614df1b02465093f431576
Yihui Xiong <xiongyihui3@gmail.com>
2014-11-25 15:16:09 +0800

    custom pin config for ble smurfs

--- interface/interface/hal/TARGET_NXP/TARGET_LPC11U35/DAP_config.h
+++ interface/interface/hal/TARGET_NXP/TARGET_LPC11U35/DAP_config.h
@@ -117,10 +117,10 @@ Provides definitions about:
 // Debug Port I/O Pins
 // For LPC11Uxx DAPs all SWD and JTAG pins are on GPIO port 0
 // Default is mbed HDK reference design with LPC11U35/501
-// SWCLK/TCK Pin                PIO0_7
-#define PIN_SWCLK_IN_BIT        7
+// SWCLK/TCK Pin                PIO0_9
+#define PIN_SWCLK_IN_BIT        9
 #define PIN_SWCLK               (1 << PIN_SWCLK_IN_BIT)
-#define PIN_SWCLK_TCK_IOCON     LPC_IOCON->PIO0_7
+#define PIN_SWCLK_TCK_IOCON     LPC_IOCON->PIO0_9
 
 // SWDIO/TMS In/Out Pin         PIO0_8
 #define PIN_SWDIO_IN_BIT        8
